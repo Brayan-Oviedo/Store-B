e97ffbf05e855cd3d946a0f8f41770f57ca9809e
x-Fantasma <xbrax27@gmail.com>
2021-09-26 23:06:38 -0500

    Se implementa creacion y cancelacion de ordenes de venta.

--- src/main/java/co/store/Init.java
+++ src/main/java/co/store/Init.java
@@ -25,7 +25,7 @@ public class Init implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
-		// PRODUCTS
+		/*// PRODUCTS
 		Product P1 = new Product("P1", "T-SHIRT", 5, Categories.T_SHIRT.toString(), 1000, 1500);
 		Product P2 = new Product("P2", "JACKET", 5, Categories.JACKET.toString(), 1000, 1500);
 		Product P3 = new Product("P3", "COAT", 5, Categories.COAT.toString(), 1000, 1500);
@@ -37,14 +37,14 @@ public class Init implements CommandLineRunner{
 		
 /** ----------ESTO SOLO SE DEBE EJEUTAR UNA VEZ---------- **/
 		
-        productService.saveProduct(P1);
+       /* productService.saveProduct(P1);
         productService.saveProduct(P2);
         productService.saveProduct(P3);
         productService.saveProduct(P4);
         
         cashService.saveCash(cash);
         
-        
+        */
 	}
 
 }
