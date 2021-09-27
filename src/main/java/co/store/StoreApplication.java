130d50ae697ed829ad810030f11aee4a4e8a94f3
x-Fantasma <xbrax27@gmail.com>
2021-09-26 23:27:31 -0500

    Se implementa creacion y cancelacion de ordenes de venta.

--- src/main/java/co/store/StoreApplication.java
+++ src/main/java/co/store/StoreApplication.java
@@ -2,8 +2,10 @@ package co.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.yawintutor.controller"})
 public class StoreApplication {
 
 	public static void main(String[] args) {
